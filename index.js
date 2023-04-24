5ea3bdc252cd793b18f9d729a0009f899e86f45c
Facundo Zanin <facundozanin@gmail.com>
2023-04-24 01:52:43 -0300

    corazon andando

--- index.js
+++ index.js
@@ -8,94 +8,6 @@ const zapatillas = [
   { id:"7-Vapormax", tipo:"Zapatillas", genero:"Hombre", nombre: "Vapormax", marca: "Nike", precio: 55000, imagen:"archivos/imagenes de zapatillas/Zapatillas Nike Vapormax.jpeg" }
 ];
 
-/* Funciones proximas para usar
-function agregarZapatillas(zapatillas) {
-  while (true) {
-    const nombre = prompt("Ingresa el nombre de la zapatilla:");
-    const marca = prompt("Ingresa la marca de la zapatilla:");
-    const precio = parseInt(prompt("Ingresa el precio de la zapatilla:"));
-    const zapatilla = {
-      nombre,
-      marca,
-      precio
-    };
-    zapatillas.push(zapatilla);
-    const continuar = confirm("¿Quieres agregar otra zapatilla?");
-    if (!continuar) {
-      break;
-    }
-  }
-  return zapatillas;
-}
-
-function buscarZapatilla() {
-  console.log("Lista de zapatillas:");
-  console.log(zapatillas);
-  const nombreBuscado = prompt("Ingresa el nombre de la zapatilla que quieres buscar:");
-  const objetoBuscado = buscarObjeto(nombreBuscado, zapatillas);
-  if (objetoBuscado === null) {
-    console.log(`No se encontró ninguna zapatilla con el nombre "${nombreBuscado}"`);
-  } else {
-    const cuotas3 = calcularCuotas(objetoBuscado, 3);
-    console.log(`Precio de la zapatilla "${objetoBuscado.nombre}" en 3 cuotas: $${cuotas3.precioCuota.toFixed(2)} por cuota. Precio total: $${cuotas3.precioTotal.toFixed(2)}.`);
-    
-    const cuotas6 = calcularCuotas(objetoBuscado, 6);
-    console.log(`Precio de la zapatilla "${objetoBuscado.nombre}" en 6 cuotas: $${cuotas6.precioCuota.toFixed(2)} por cuota. Precio total: $${cuotas6.precioTotal.toFixed(2)}.`);
-  }
-}
-
-function buscarObjeto(nombreObjeto, arrayObjetos) {
-  // Recorremos el array de objetos
-  for (let i = 0; i < arrayObjetos.length; i++) {
-    // Si encontramos el objeto buscado, lo retornamos
-    if (arrayObjetos[i].nombre.toLowerCase() === nombreObjeto.toLowerCase()) {
-      return arrayObjetos[i];
-    }
-  }
-  // Si no se encuentra el objeto, retornamos null
-  return null;
-}
-
-function calcularCuotas(objeto, cuotas) {
-  // Calculamos el precio con el 5% de interés mensual
-  const precioConInteres = objeto.precio * (1 + 0.05) ** cuotas;
-  // Calculamos el precio de cada cuota
-  const precioCuota = precioConInteres / cuotas;
-  // Retornamos los resultados
-  return {
-    cuotas,
-    precioCuota,
-    precioTotal: precioConInteres
-  };
-}
-*/
-
-const productGrid = document.getElementById('productGrid');
-
-zapatillas.forEach(zapatilla => {
-  const productCard = document.createElement('div');
-  productCard.classList.add('col-lg-4', 'col-md-6', 'col-sm-12', 'product-card');
-
-  const image = document.createElement('img');
-  image.src = zapatilla.imagen;
-  image.alt = zapatilla.nombre;
-  productCard.appendChild(image);
-
-  const productName = document.createElement('h2');
-  productName.textContent = zapatilla.nombre;
-  productCard.appendChild(productName);
-
-  const productBrand = document.createElement('p');
-  productBrand.textContent = zapatilla.marca;
-  productCard.appendChild(productBrand);
-
-  const productPrice = document.createElement('p');
-  productPrice.classList.add('price');
-  productPrice.textContent = `$${zapatilla.precio}`;
-  productCard.appendChild(productPrice);
-});
-
-
 // CODIGO DEL FILTRO !!!!!!
 // Obtener los elementos HTML relevantes
 const chkZapatillas = document.getElementById("chkZapatillas");
@@ -176,63 +88,110 @@ function filtrar() {
     return cumpleFiltro;
   });
 
-  // Generar las tarjetas de productos filtrados
-productGridNuevo.innerHTML = "";
-productosFiltrados.forEach((producto) => {
-  const card = `
-    <div class="col-lg-4 col-md-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="${producto.imagen}" alt=""></a>
-        <div class="card-body">
-          <p class="card-title">
-            <a href="#" class="product-link" data-id="${producto.id}">${producto.nombre}</a>
-          </p>
-          <div class="d-flex justify-content-between"><h4>$${producto.precio}</h4>   
-
-          <button class="heart-icon" data-id="${producto.id}"</button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-            </svg>
+  //  Generar la tarjeta con los productos
+  productGridNuevo.innerHTML = "";
+  productosFiltrados.forEach((producto) => {
+    const card = `
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="${producto.imagen}" alt=""></a>
+          <div class="card-body">
+            <p class="card-title">
+              <a href="#" class="product-link" data-id="${producto.id}">${producto.nombre}</a>
+            </p>
+            <div class="d-flex justify-content-between"><h4>$${producto.precio}</h4>
+            <div class="heart-icon" data-id="${producto.id}">
+              <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+              </svg>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  `;
-  productGrid.innerHTML += card;
-});
-
-// Obtener todos los enlaces de productos y añadir un event listener
-const productLinks = document.querySelectorAll('.product-link');
-productLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    const id = link.getAttribute('data-id');
-    window.location.href = `producto-${id}.html`;
+    `;
+    productGrid.innerHTML += card;
   });
-});
+  
+  // Obtener todos los enlaces de productos y añadir un event listener
+  const productLinks = document.querySelectorAll('.product-link');
+  productLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const id = link.getAttribute('data-id');
+      window.location.href = `producto-${id}.html`;
+    });
+  });
+  
+  // Obtener todos los iconos de corazón y añadir un event listener
+  const heartIcons = document.querySelectorAll('.heart-icon');
 
-// Obtener todos los iconos de corazón y añadir un event listener
-const heartIcons = document.querySelectorAll('.heart-icon');
-heartIcons.forEach(icon => {
-  icon.addEventListener('click', (event) => {
+    // Agregar evento click a cada corazón
+    heartIcons.forEach(icon => {
+    icon.addEventListener('click', (event) => {
     event.preventDefault();
     const id = icon.getAttribute('data-id');
-    const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+
+    // Si el producto ya está en la lista de favoritos, removerlo y cambiar color del corazón a su estado original
     if (!favoritos.includes(id)) {
       favoritos.push(id);
       localStorage.setItem('favoritos', JSON.stringify(favoritos));
+      icon.classList.replace('heart-icon', 'heart-icon--active');
+      icon.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16" style="fill: red;">
+          <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+        </svg>
+      `;
+    } else {
+      const index = favoritos.indexOf(id);
+      favoritos.splice(index, 1);
+      localStorage.setItem('favoritos', JSON.stringify(favoritos));
+      icon.classList.replace('<bottonheart-icon--active', 'heart-icon');
+      icon.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+        </svg>
+      `;
     }
-    icon.classList.add('heart-icon--active');
   });
 });
 
-}
+// Mantener los elementos guardados en favoritos aun refrescando la pagina
+window.addEventListener('load', () => {
+  const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+  const heartIcons = document.querySelectorAll('.heart-icon');
+  heartIcons.forEach(icon => {
+    const id = icon.getAttribute('data-id');
+    if (favoritos.includes(id)) {
+      icon.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16" style="fill: red;">
+          <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+        </svg>
+      `;
+      icon.classList.add('heart-icon--active');
+    }
+  });
+});
+
+
+// Agregar evento DOMContentLoaded para verificar si los productos están en la lista de favoritos y cambiar el color del corazón si es necesario
+document.addEventListener('DOMContentLoaded', () => {
+  const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+  favoritos.forEach((favorito) => {
+    const icon = document.querySelector(`.heart-icon[data-id="${favorito}"]`);
+    icon.classList.add('heart-icon--active');
+  });
+});
 
 // Muestra el precio actual de lo que vale el en el filtro
 const rangePrecioValor = document.getElementById('rangePrecioValor');
 rangePrecio.addEventListener('input', () => {
   rangePrecioValor.textContent = rangePrecio.value.toLocaleString();
 });
+
+
+}
+
 
 // Mostrar todos los productos al cargar la página
 filtrar();
